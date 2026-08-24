6a8588076fb409305e4303ea0197dc768e3cf67c
Lumio111 <lomax.a.b@gmail.com>
2026-08-24 15:59:54 +0300

    Исправил кэш и обновил файлы

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,13 @@
-﻿   // Service Worker отключён
+﻿const CACHE_NAME = 'lumio-v4';
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(c => {
+            return c.addAll(['/', '/index.html', '/app.js']);
+        })
+    );
+});
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
