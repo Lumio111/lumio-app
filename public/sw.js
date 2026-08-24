192bd085f143dbd570dc1178d7e60f1c79066e6b
Lumio111 <lomax.a.b@gmail.com>
2026-08-25 00:08:36 +0300

    Исправил sw.js и manifest для APK

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,9 @@
-﻿   const CACHE_NAME = 'lumio-v7';
-   self.addEventListener('install', (e) => {
-       e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['/', '/index.html', '/app.js', '/manifest.json'])));
-   });
-   self.addEventListener('fetch', (e) => {
-       e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-   });
+﻿const CACHE_NAME = 'lumio-v8';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => 
+    c.addAll(['/', '/index.html', '/app.js', '/manifest.json'])
+  ));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
