fd1af7cf52b1b415060caf4b5c1aca69bf56293f
Lumio111 <lomax.a.b@gmail.com>
2026-08-24 16:54:27 +0300

    Nuclear SW update v100

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,29 @@
-﻿const CACHE_NAME = 'lumio-v4';
+﻿const CACHE_NAME = 'lumio-v100-FINAL';
+
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(c => {
-            return c.addAll(['/', '/index.html', '/app.js']);
-        })
-    );
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['/', '/index.html', '/app.js', '/manifest.json']);
+    }).then(() => self.skipWaiting()) // Принудительно активирует новый SW
+  );
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key); // УДАЛЯЕТ все старые версии (v1, v3 и т.д.)
+        }
+      }));
+    }).then(() => self.clients.claim()) // Принудительно берет контроль над всеми вкладками
+  );
+});
+
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(r => r || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
