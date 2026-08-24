a8e0883873e104b574d9394c98b20b6d0618114d
Lumio111 <lomax.a.b@gmail.com>
2026-08-24 23:53:01 +0300

    изменение настроек

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,7 @@
-﻿const CACHE_NAME = 'lumio-v6';
+﻿   const CACHE_NAME = 'lumio-v7';
+   self.addEventListener('install', (e) => {
+       e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['/', '/index.html', '/app.js', '/manifest.json'])));
+   });
+   self.addEventListener('fetch', (e) => {
+       e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+   });
